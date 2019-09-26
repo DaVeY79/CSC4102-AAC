6ed3a65c0973fc3e68989cc658bf24e53d309e59
David Abraham <daveynoire97@gmail.com>
2019-09-26 13:18:23 +0530

    fixed count sort program to include 0 values while sorting

--- count_sort.py
+++ count_sort.py
@@ -1,14 +1,14 @@
 def countsort(A):
     k = max(A)
     n = len(A)
-    A = [None,*A]
-    B = [None]+([0]*n)
-    C = [None]+([0]*k)
-    for i in range(1,n+1):
+    #A = [None,*A]
+    B = ([0]*(n+1))
+    C = ([0]*(k+1))
+    for i in range(0,n):
         C[A[i]] = C[A[i]]+1
-    for i in range(2,k+1):
+    for i in range(1,k+1):
         C[i] = C[i]+C[i-1]
-    for i in range(n,0,-1):
+    for i in range(n-1,-1,-1):
         B[C[A[i]]] = A[i]
         C[A[i]] = C[A[i]] - 1
     return B[1:]
